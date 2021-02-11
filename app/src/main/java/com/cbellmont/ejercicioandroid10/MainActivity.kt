a692870dcafa1f05fb2ef16c5dbaac7d0a13a47a
Carlos de Tena Bellmont <carlos.bellmont@gmail.com>
2021-02-11 15:31:00 +0100

    Updated to 30

--- app/src/main/java/com/cbellmont/ejercicioandroid10/MainActivity.kt
+++ app/src/main/java/com/cbellmont/ejercicioandroid10/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.cbellmont.ejercicioandroid10
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.cbellmont.ejercicioandroid10.databinding.ActivityMainBinding
+import com.cbellmont.ejercicioandroid10.databinding.ItemLayoutBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val personaje1 = Personaje("Aragorn", "Humano", R.mipmap.aragorn, true)
         val personaje2 = Personaje("Gandalf", "Mago", R.mipmap.gandalf, true)
@@ -20,23 +23,23 @@ class MainActivity : AppCompatActivity() {
         val personaje5 = Personaje("Orco Feo", "Orco", R.mipmap.orco, false)
         val personaje6 = Personaje("Smagu", "Dragon", R.mipmap.smagu, false)
 
-        fillView(findViewById(R.id.layoutPersonaje1), personaje1)
-        fillView(findViewById(R.id.layoutPersonaje2), personaje2)
-        fillView(findViewById(R.id.layoutPersonaje3), personaje3)
-        fillView(findViewById(R.id.layoutPersonaje4), personaje4)
-        fillView(findViewById(R.id.layoutPersonaje5), personaje5)
-        fillView(findViewById(R.id.layoutPersonaje6), personaje6)
+        fillView(binding.layoutPersonaje1, personaje1)
+        fillView(binding.layoutPersonaje2, personaje2)
+        fillView(binding.layoutPersonaje3, personaje3)
+        fillView(binding.layoutPersonaje4, personaje4)
+        fillView(binding.layoutPersonaje5, personaje5)
+        fillView(binding.layoutPersonaje6, personaje6)
     }
 
-    private fun fillView(parent : LinearLayout, personaje: Personaje) {
-        val color = if (personaje.esBueno) R.color.colorBgBueno else R.color.colorBgMalo
-        parent.setBackgroundColor(ContextCompat.getColor(this, color))
-        val iwFoto = parent.findViewById<ImageView>(R.id.iwFoto)
-        val twNombre = parent.findViewById<TextView>(R.id.tvNombre)
-        val twRaza = parent.findViewById<TextView>(R.id.tvRaza)
-        iwFoto.setImageResource(personaje.image)
-        twNombre.text = personaje.nombre
-        twRaza.text = personaje.raza
+    private fun fillView(item: ItemLayoutBinding, personaje: Personaje) {
+        val colorId = if (personaje.esBueno) R.color.colorBgBueno else R.color.colorBgMalo
+        // Utilizamos ContextCompat.getColor(this, color) para obtener un color, hasta ahora solo
+        // teníamos un id del color
+        val color = ContextCompat.getColor(this, colorId)
+        item.root.setBackgroundColor(color)
+        item.iwFoto.setImageResource(personaje.image)
+        item.tvNombre.text = personaje.nombre
+        item.tvRaza.text = personaje.raza
     }
 
 }
